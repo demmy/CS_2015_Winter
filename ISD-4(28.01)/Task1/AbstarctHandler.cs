9fc4cc163b820a646a25c9582c17e09b5db22cee
braginskyy <braginskyy_oleg@ukr.net>
2016-01-28 11:10:40 +0200

    add thetd task

---
+++ ISD-4(28.01)/Task1/AbstarctHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public abstract class AbstarctHandler
+    {
+        public abstract void Open();
+        public abstract void Create();
+        public abstract void Change();
+        public abstract void Save();
+    }
+}
---
+++ ISD-4(28.01)/Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player p = new Player();
+            p.Play();
+            p.Record();
+            p.Pause();
+            ((IPlayable)p).Stop();
+            ((IRecordable)p).Stop();
+            Console.ReadLine();
+        }
+    }
+}
