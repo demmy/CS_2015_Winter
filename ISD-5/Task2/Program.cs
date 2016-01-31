d82ef03e560194d9b2ba82400d3d8d29d99fd71a
braginskyy <braginskyy_oleg@ukr.net>
2016-01-31 15:35:41 +0200

    01.02.2016

---
+++ ISD-5/Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mass = { 11, 5, 7, 9, 4, 15, 3, 22 };
+            Console.WriteLine("\nBefore sort");
+            for (int i = 0; i < mass.Length; i++)
+            {
+                Console.Write("{0},", mass[i]);
+            }
+            Console.WriteLine();
+            mass.MySort();
+            Console.WriteLine("\nAfter sort");
+            for (int i = 0; i < mass.Length; i++)
+            {
+                Console.Write("{0},", mass[i]);
+            }
+            Console.WriteLine();
+            MySortArray.MySort(mass, false);
+            Console.WriteLine("\nAfter sort with trend");
+            for (int i = 0; i < mass.Length; i++)
+            {
+                Console.Write("{0},", mass[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ISD-5/Task3/MySortA.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    static class MySortA
+    {
+        public static int[] MySort(this int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[i])
+                    {
+                        int temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+        public static 
+    }
+}
