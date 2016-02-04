ab6218180a5169c2a58d16a254910f96ba6e3dab
braginskyy <braginskyy_oleg@ukr.net>
2016-02-04 03:33:53 +0200

    04.04.2016

---
+++ ISD-7/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        delegate int MyDelegate(int x, int y, int z);
+        static void Main(string[] args)
+        {
+            int average;
+            MyDelegate Exampl = delegate(int x, int y, int z) {return (x+y+z)/3 ;};
+            average = Exampl(3,6,8);
+            Console.WriteLine(average);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ISD-7/Task3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public delegate int Exampl();
+    public delegate double Anonymus(Exampl[] d);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+            Exampl[] d = new Exampl[100];
+            for (int i = 0; i < d.Length; i++)
+            {
+                d[i] = new Exampl(Ran);                
+            }
+            Anonymus anonimousDelegat;
+            anonimousDelegat = (mass) =>
+            {                
+                for (int i = 0; i < mass.Length; i++)
+                {
+                    sum = sum + (double)mass[i]()/mass.Length;                    
+                }
+                return sum;                
+            };
+            anonimousDelegat(d);
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+        static int Ran()
+        {
+            Random r = new Random();
+            return r.Next(1, 100);
+        }
+    }
+}
