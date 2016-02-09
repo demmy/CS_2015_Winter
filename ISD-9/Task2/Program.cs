ca9afecd51eef5892afa4dfd59de97142ff47d97
braginskyy <braginskyy_oleg@ukr.net>
2016-02-09 02:16:52 +0200

    09.02.2016

---
+++ ISD-9/Task2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> list = new MyList<int>();
+            list.Add(1);
+            list.Add(3);
+            list.Add(4);
+            list.Add(9);
+            list.Add(9);
+            list.Add(4);
+            list.Add(2);
+            list.Add(7);
+            list[5] = 12;
+            Console.WriteLine("Element [4]   =   {0}", list[4]);
+            Console.WriteLine("List length   =   {0}", list.Count);
+            foreach (int i in list.GetList())
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("--------------------------------------------------");
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list.GetArray()[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
