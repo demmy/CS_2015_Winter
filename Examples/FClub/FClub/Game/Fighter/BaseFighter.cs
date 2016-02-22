04a6dd6bd1a05ebab99a9c32f828ed8be53ff7c7
demmy <demiurge.ds@gmail.com>
2016-02-22 20:05:48 +0200

    user control sample

---
+++ Examples/FClub/FClub/Game/Fighter/BaseFighter.cs
@@ -0,0 +1,55 @@
+﻿namespace FClub.Game.Fighter
+{
+    abstract class BaseFighter : IFighter
+    {
+        public BaseFighter(string name, int hp)
+        {
+            this.name = name;
+            this.hp = hp;
+        }
+
+        public BaseFighter()
+        {
+
+        }
+
+        public event FighterEventHandler WasBlocked;
+
+        public event FighterEventHandler WasDamaged;
+
+        public event FighterEventHandler WasKilled;
+
+        protected void Blocked()
+        {
+            if (WasBlocked != null)
+            {
+                WasBlocked(this, new FighterEventArgs() { Name = this.Name, HP = this.HP});
+            }
+        }
+
+        int hp = 0;
+        public int HP
+        {
+            get { return hp; }
+        }
+
+        string name = string.Empty;
+        public string Name
+        {
+            get { return name; }
+        }
+
+        Part part = Part.Head;
+        public Part BlockedPart
+        {
+            get { return part; }
+            protected set { part = value; }
+        }
+
+        public abstract void GetHit(Part part);
+
+
+        public abstract void SetBlock(Part part);
+
+    }
+}
---
+++ Examples/FClub/FClub/Game/Fighter/HumanFighter.cs
@@ -0,0 +1,27 @@
+﻿namespace FClub.Game.Fighter
+{
+    internal class HumanFighter : BaseFighter
+    {
+        public override void GetHit(Part part)
+        {
+            if (BlockedPart == part)
+            {
+                // hit blocked
+                Blocked();
+            }
+            else
+            {
+                //get damage
+            }
+        }
+
+
+
+
+
+        public override void SetBlock(Part part)
+        {
+            BlockedPart = part;
+        }
+    }
+}
