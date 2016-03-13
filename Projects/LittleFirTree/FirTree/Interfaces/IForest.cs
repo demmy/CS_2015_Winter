33561a5cc9df0a489d4cabd362ee5ae36e773e58
Pikovit44 <Pikovit44@gmail.com>
2016-03-13 12:45:29 +0200

    + FirTree project

---
+++ Projects/LittleFirTree/FirTree/Interfaces/IForest.cs
@@ -0,0 +1,14 @@
+﻿using Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirTree.Interfaces
+{
+    public interface IForest
+    {
+        PartOfYear ActualSeason { get; }
+    }
+}
