d43e76b9e1ebcb01acb57be56fa5eb495e333ca7
braginskyy <braginskyy_oleg@ukr.net>
2016-03-13 19:48:41 +0200

    FirTree
    First TDD project

---
+++ Project/ISD-12/ISD-12/IFirTree.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISD_12
+{
+    public interface IFirTree
+    {
+        int GrowFirTree(int increase);
+        int Hight { get; }
+        string Form { get; }
+        string Color { get; }
+    }
+}
