d5339f07ec591ed2fed29db7dec01cf7a65253c6
Pikovit44 <Pikovit44@gmail.com>
2016-03-14 11:53:31 +0200

    NextSeason() -> ChooseSeason() + IfExistGrowth();

--- Projects/LittleFirTree/FirTree/Forest.cs
+++ Projects/LittleFirTree/FirTree/Forest.cs
@@ -37,6 +37,12 @@ namespace FirTree
         public void NextSeason()
         {
             age += DeltaAge;
+            ChooseSeason();
+            IfExistGrowth();
+        }
+
+        private void ChooseSeason()
+        {
             switch (actualSeason)
             {
                 case PartOfYear.Spring:
@@ -57,12 +63,14 @@ namespace FirTree
                 default:
                     break;
             }
+        }
 
+        private void IfExistGrowth()
+        {
             if (firTree != null)
             {
                 firTree.Growth();
             }
-            
         }
 
         public Shape GetFormFirTree()
@@ -97,8 +105,7 @@ namespace FirTree
             
             Console.ReadLine();
         }
-
-
+        
         static string GetDescription(Enum value)
         {
             FieldInfo fi = value.GetType().GetField(value.ToString());
