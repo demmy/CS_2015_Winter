16767b815800b9bdb59c754df0f6e58bd6752dd2
braginskyy <braginskyy_oleg@ukr.net>
2016-03-22 16:25:37 +0200

    Add New EF Project
    First part. Create repository

---
+++ Project/ISD-13/ISD-13/Repository/Interfaces/IPlayerRepository.cs
@@ -0,0 +1,15 @@
+﻿using ISD_13.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISD_13.Repository.Interfaces
+{
+    interface IPlayerRepository : IRepository<Player>
+    {
+        IEnumerable<Player> FindRegisteredUsersByDate(DateTime date);
+        IEnumerable<Player> FindUsersByValidEmail();
+    }
+}
