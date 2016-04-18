ee87ab6a1a7d7ec26804c22e4ee504d889746630
Евгений <jack-carver@bk.ru>
2016-04-18 15:53:53 +0300

    add FightClubStatistics project

---
+++ HomeWorks/Analitycs/Syrnikov/FightClubStatistics/FightClubStatistics/Data/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightClubStatistics
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string EMail { get; set; }
+        public virtual Player PlayerData { get; set; }
+        public bool IsEmailValid { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
---
+++ HomeWorks/Analitycs/Syrnikov/FightClubStatistics/FightClubStatistics/Repository/UserRepository.cs
@@ -0,0 +1,76 @@
+﻿using FightClubStatistics.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightClubStatistics
+{
+    class UserRepository : AbstractRepository, IRepository<User> , IUserRepository
+    {
+        public UserRepository(MyContext context)
+        {
+            this.context = context;
+        }
+
+        public void Add(User item)
+        {
+            context.Users.Add(item);
+            context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            User user = context.Users.Find(id);
+            if (user != null)
+            {
+                context.Users.Remove(user);
+            }
+            context.SaveChanges();
+        }
+
+        public User Get(int id)
+        {
+            return context.Users.Find(id);
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            return context.Users;
+        }
+
+        public void Update(User item)
+        {
+            User oldUser;
+            oldUser = context.Users.Where(x => x.UserId == item.UserId).FirstOrDefault();
+            oldUser.Login = item.Login;
+            oldUser.Password = item.Password;
+            oldUser.EMail = item.EMail;
+            oldUser.IsEmailValid = item.IsEmailValid;
+            oldUser.PlayerData.Name = item.PlayerData.Name;
+            oldUser.PlayerData.Exp = item.PlayerData.Exp;
+            context.SaveChanges();
+        }
+
+        public IEnumerable<User> FindRegisteredUsersByDate(DateTime date)
+        {
+            return GetAll().Where(user => user.CreationDate == date).ToList();
+        }
+
+        public IEnumerable<User> FindUsersWithValidEMail()
+        {
+            return GetAll().Where(user => user.IsEmailValid).ToList();
+        }
+
+        public IEnumerable<User> GetAllWithValidEmail()
+        {
+            return context.Users.Where(x => x.IsEmailValid);
+        }
+
+        public IEnumerable<User> GetAllWithoutValidEmail()
+        {
+            return context.Users.Where(x => !x.IsEmailValid);
+        }
+    }
+}
