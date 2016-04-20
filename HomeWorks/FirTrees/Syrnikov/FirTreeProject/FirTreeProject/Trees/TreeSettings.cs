0e8013d602d3bba31c731afd8bfc2733e86be4a3
Евгений <jack-carver@bk.ru>
2016-04-20 15:02:37 +0300

    work on bugs in FirTreeProject

---
+++ HomeWorks/FirTrees/Syrnikov/FirTreeProject/FirTreeProject/Trees/TreeSettings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirTreeProject
+{
+    public enum TreeColor
+    {
+        Green,
+        Yellow,
+        Red,
+        Brown,
+        LightGreen,
+        DarkGreen
+    }
+    public enum TreeShape
+    {
+        Shapely,
+        Wide
+    }
+    //class TreeSettings
+    //{
+    //}
+}
---
+++ HomeWorks/FirTrees/Syrnikov/FirTreeProject/FirTreeProjectTests/FirTreeTests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using FirTreeProject;
+
+namespace FirTreeProjectTests
+{
+    [TestFixture]
+    class FirTreeTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void FirTreeBornAndGrow()
+        {
+            //arrange
+            FirTree firTree = new FirTree();
+
+            //act
+            firTree.GrowUp(10.0);
+
+            //assert
+            Assert.AreEqual(10.0 , firTree.Height);
+        }
+
+        [Test]
+        public void ChangeGrowthSpeed()
+        {
+            //arrange
+            FirTree firTree = new FirTree();
+
+            //act
+            firTree.GrowUp(10.0);
+            firTree.GrowthSpeed = 1.3;
+            firTree.GrowUp();
+
+            //assert
+            Assert.AreEqual(11.3, firTree.Height);
+        }
+
+        [Test]
+        public void ColorOfFirTreeIsGreen()
+        {
+            //arrange
+            FirTree firTree = new FirTree();
+
+            //act
+
+            //assert
+            Assert.AreEqual(TreeColor.Green, firTree.Color);
+        }
+
+        [Test]
+        public void InWinterGreenAndShapely()
+        {
+            //arrange
+            Forest testForest = new Forest();
+            testForest.AddTree(new FirTree());
+
+            //act
+            while (NatureEnvironment.CurrentSeason != Season.Winter)
+            {
+                NatureEnvironment.ChangeSeason();
+            }
+
+            //assert
+            Assert.AreEqual(Season.Winter, NatureEnvironment.CurrentSeason);
+            Assert.AreEqual(TreeShape.Shapely , testForest.Trees.LastOrDefault(x => x is FirTree).Shape);
+            Assert.AreEqual(TreeColor.Green, testForest.Trees.LastOrDefault(x => x is FirTree).Color);
+        }
+
+        [Test]
+        public void InSummerGreenAndShapely()
+        {
+            //arrange
+            Forest testForest = new Forest();
+            testForest.AddTree(new FirTree());
+
+            //act
+            NatureEnvironment.ChangeSeason();
+            NatureEnvironment.ChangeSeason();
+
+            //assert
+            Assert.AreEqual(Season.Summer, NatureEnvironment.CurrentSeason);
+            Assert.AreEqual(TreeShape.Shapely, testForest.Trees.LastOrDefault(x => x is FirTree).Shape);
+            Assert.AreEqual(TreeColor.Green, testForest.Trees.LastOrDefault(x => x is FirTree).Color);
+        }
+    }
+}
